8abf0402a1c63080dd4bd5f4c2a4db7bb8a6de44
Andrii Veremii <dashuk10@gmail.com>
2024-02-23 23:25:15 +0100

    add

--- src/components/WeatherWeeck/WeatherWeeck.jsx
+++ src/components/WeatherWeeck/WeatherWeeck.jsx
@@ -3,22 +3,21 @@ import { WeatherWeeckItem } from 'components/WeeckWrapperItem/WeeckWrapperItem';
 
 export const WeatherWeeck = ({ weathers }) => {
   const { list } = weathers;
-console.log(list)
   return (
     <ul>
-      {list && list.map(item => {
-
-        console.log(item.main.dt_txt)
-
-        return <li key={item.main.dt_txt} item={item}>
-          <img
-            src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-            style={{ height: '50px', width: '50px' }}
-            alt="icon"
-          />
-          <span>Теспература: {Math.round(item.main.temp)}°C</span>
-          </li>;
-      })}
+      {list &&
+        list.map(item => {
+          return (
+            <li key={item.main.dt_txt} item={item}>
+              <img
+                src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                style={{ height: '50px', width: '50px' }}
+                alt="icon"
+              />
+              <span>Теспература: {Math.round(item.main.temp)}°C</span>
+            </li>
+          );
+        })}
     </ul>
   );
 };
